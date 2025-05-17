e6be530c5a0cbbd417afd5df232245d4266127f7
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-17 10:35:03 -0400

    Update item_storage_module_1.lua
    Revert

--- MMOCoreORB/bin/scripts/object/tangible/component/droid/item_storage_module_1.lua
+++ MMOCoreORB/bin/scripts/object/tangible/component/droid/item_storage_module_1.lua
@@ -50,7 +50,7 @@ object_tangible_component_droid_item_storage_module_1 = object_tangible_componen
 	experimentalGroupTitles = {"null", "null", "exp_durability", "null", "exp_effectiveness", "exp_effectiveness"},
 	experimentalSubGroupTitles = {"null", "null", "decayrate", "hitpoints", "mechanism_quality", "storage_module"},
 	experimentalMin = {0, 0, 5, 1000, -10, 1},
-	experimentalMax = {0, 0, 15, 1000, 15, 10},
+	experimentalMax = {0, 0, 15, 1000, 15, 3},
 	experimentalPrecision = {0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 4, 1, 1},
 }
