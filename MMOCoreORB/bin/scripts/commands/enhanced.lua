9fca2d1af181d39c10425e38de085d73536b3909
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-17 12:52:03 -0400

    Fix

--- MMOCoreORB/bin/scripts/commands/enhanced.lua
+++ MMOCoreORB/bin/scripts/commands/enhanced.lua
@@ -1,19 +1,14 @@
-function enhanceCommand(pPlayer, args)
-	if (pPlayer == nil) then
+function enhanceCommand(screenPlay, player, args)
+	if (player == nil) then
 		return 0
 	end
 
-	local creature = LuaCreatureObject(pPlayer)
-
-	-- Health (0, 1, 2), Action (3, 4, 5), Mind (6, 7, 8)
-	for i = 0, 8 do
-		local maxHam = creature:getMaxHAM(i)
-		creature:setHAM(i, maxHam)
-	end
-
+	local creature = CreatureObject(player)
+	creature:setHAM(creature:getMaxHAM(0), creature:getMaxHAM(1), creature:getMaxHAM(2))
+	creature:setHAM(creature:getMaxHAM(3), creature:getMaxHAM(4), creature:getMaxHAM(5))
+	creature:setHAM(creature:getMaxHAM(6), creature:getMaxHAM(7), creature:getMaxHAM(8))
 	creature:sendSystemMessage("You have been fully enhanced.")
-
 	return 0
 end
 
-registerCreatureCommand("enhance", enhanceCommand)
+registerSlashCommand("enhance", "enhanceCommand")
