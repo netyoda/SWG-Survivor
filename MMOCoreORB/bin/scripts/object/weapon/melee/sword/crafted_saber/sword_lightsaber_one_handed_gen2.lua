8b3f4bd1a5a7cce01562b3bad538045c7649f913
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-17 09:01:48 -0400

    Update sword_lightsaber_one_handed_gen2.lua
    healthAttackCost = 47,
    	actionAttackCost = 55,
    	mindAttackCost = 25,
    	forceCost = 24,
    
    	pointBlankRange = 0,
    	pointBlankAccuracy = 20,
    
    	idealRange = 3,
    	idealAccuracy = 15,
    
    	maxRange = 5,
    	maxRangeAccuracy = 5,
    
    	minDamage = 125,
    	maxDamage = 215,
    
    	attackSpeed = 5.1,
    
    	woundsRatio = 19,
    
    	defenderToughnessModifiers = { "lightsaber_toughness" },
    
    	noTrade = 1,
    	
    	childObjects = {
    		{templateFile = "object/tangible/inventory/lightsaber_inventory_2.iff", x = 0, z = 0, y = 0, ox = 0, oy = 0, oz = 0, ow = 0, cellid = -1, containmentType = 4}
    	},
    
    	numberExperimentalProperties = {1, 1, 2, 2, 2, 2, 2, 1, 1, 1},
    	experimentalProperties = {"XX", "XX", "CD", "OQ", "CD", "OQ", "CD", "OQ", "SR", "UT", "CD", "OQ", "OQ", "OQ", "OQ"},
    	experimentalWeights = {1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1},
    	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "expEffeciency", "expEffeciency", "expEffeciency", "expEffeciency"},
    	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "forcecost", "attackhealthcost", "attackactioncost", "attackmindcost"},
    	experimentalMin = {0, 0, 125, 215, 5.1, 13, 28, 47, 55, 25},
    	experimentalMax = {0, 0, 145, 255, 4.8, 25, 24, 32, 40, 20},
    	experimentalPrecision = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
    	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
    }

--- MMOCoreORB/bin/scripts/object/weapon/melee/sword/crafted_saber/sword_lightsaber_one_handed_gen2.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/sword/crafted_saber/sword_lightsaber_one_handed_gen2.lua
@@ -99,10 +99,10 @@ object_weapon_melee_sword_crafted_saber_sword_lightsaber_one_handed_gen2 = objec
 
 
 	-- The values below are the default values.  To be used for blue frog objects primarily
-	healthAttackCost = 35,
-	actionAttackCost = 40,
-	mindAttackCost = 20,
-	forceCost = 12,
+	healthAttackCost = 47,
+	actionAttackCost = 55,
+	mindAttackCost = 25,
+	forceCost = 24,
 
 	pointBlankRange = 0,
 	pointBlankAccuracy = 20,
@@ -113,19 +113,19 @@ object_weapon_melee_sword_crafted_saber_sword_lightsaber_one_handed_gen2 = objec
 	maxRange = 5,
 	maxRangeAccuracy = 5,
 
-	minDamage = 105,
-	maxDamage = 195,
+	minDamage = 125,
+	maxDamage = 215,
 
 	attackSpeed = 5.1,
 
-	woundsRatio = 15,
+	woundsRatio = 19,
 
 	defenderToughnessModifiers = { "lightsaber_toughness" },
-	
-	noTrade = 1,
 
+	noTrade = 1,
+	
 	childObjects = {
-		{templateFile = "object/tangible/inventory/lightsaber_inventory_1.iff", x = 0, z = 0, y = 0, ox = 0, oy = 0, oz = 0, ow = 0, cellid = -1, containmentType = 4}
+		{templateFile = "object/tangible/inventory/lightsaber_inventory_2.iff", x = 0, z = 0, y = 0, ox = 0, oy = 0, oz = 0, ow = 0, cellid = -1, containmentType = 4}
 	},
 
 	numberExperimentalProperties = {1, 1, 2, 2, 2, 2, 2, 1, 1, 1},
@@ -133,11 +133,12 @@ object_weapon_melee_sword_crafted_saber_sword_lightsaber_one_handed_gen2 = objec
 	experimentalWeights = {1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "expEffeciency", "expEffeciency", "expEffeciency", "expEffeciency"},
 	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "forcecost", "attackhealthcost", "attackactioncost", "attackmindcost"},
-	experimentalMin = {0, 0, 105, 195, 5.1, 10, 15, 35, 40, 20},
-	experimentalMax = {0, 0, 125, 235, 4.8, 20, 12, 25, 25, 15},
+	experimentalMin = {0, 0, 125, 215, 5.1, 13, 28, 47, 55, 25},
+	experimentalMax = {0, 0, 145, 255, 4.8, 25, 24, 32, 40, 20},
 	experimentalPrecision = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
 }
+
 
 
 ObjectTemplates:addTemplate(object_weapon_melee_sword_crafted_saber_sword_lightsaber_one_handed_gen2, "object/weapon/melee/sword/crafted_saber/sword_lightsaber_one_handed_gen2.iff")
