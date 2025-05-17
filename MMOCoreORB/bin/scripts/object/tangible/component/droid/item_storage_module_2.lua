2f3ce7e2c3c3b62e82c5c5d53d9871f551caf957
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-17 08:44:16 -0400

    Update item_storage_module_2.lua
    item_storage_module_2.lua: Line 52, change the 3 to 11 and Line 53 change the 5 to 20

--- MMOCoreORB/bin/scripts/object/tangible/component/droid/item_storage_module_2.lua
+++ MMOCoreORB/bin/scripts/object/tangible/component/droid/item_storage_module_2.lua
@@ -49,8 +49,8 @@ object_tangible_component_droid_item_storage_module_2 = object_tangible_componen
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "exp_durability", "null", "exp_effectiveness", "exp_effectiveness"},
 	experimentalSubGroupTitles = {"null", "null", "decayrate", "hitpoints", "mechanism_quality", "storage_module"},
-	experimentalMin = {0, 0, 5, 1000, -10, 3},
-	experimentalMax = {0, 0, 15, 1000, 15, 5},
+	experimentalMin = {0, 0, 5, 1000, -10, 5},
+	experimentalMax = {0, 0, 15, 1000, 15, 20},
 	experimentalPrecision = {0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 4, 1, 1},
 }
